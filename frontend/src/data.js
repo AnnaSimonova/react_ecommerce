747d3ab03c03d59a69acaa72cef2dbdf6c0e73c5
AnnaSimonova <simonova.ganna@gmail.com>
2021-01-30 16:18:52 +0200

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,66 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'Nike',
+            numReviews: 10,
+            description: 'high quality product',
+        },
+        {
+            _id: '2',
+            name: 'Adidas Fit Shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 130,
+            brand: 'Nike',
+            numReviews: 10,
+            description: 'high quality product',
+        },
+        {
+            _id: '3',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 140,
+            brand: 'Nike',
+            numReviews: 10,
+            description: 'high quality product',
+        },
+        {
+            _id: '4',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 150,
+            brand: 'Nike',
+            numReviews: 10,
+            description: 'high quality product',
+        },
+        {
+            _id: '5',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 160,
+            brand: 'Nike',
+            numReviews: 10,
+            description: 'high quality product',
+        },
+        {
+            _id: '6',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 170,
+            brand: 'Nike',
+            numReviews: 10,
+            description: 'high quality product',
+        }
+    ]
+}
+
+export default data;
